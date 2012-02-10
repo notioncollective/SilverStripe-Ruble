b187edeb1934924cf901aaa26e6c390d5ccb1c7f
Jonathan Wohl <jon@jonwohl.com>
2012-02-10 11:55:05 -0500

    added ModelAdminSubclass

---
+++ templates/ModelAdminSubclassTemplate.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * ${TM_NEW_FILE_BASENAME} class
+ *
+ * Created by ${TM_FULLNAME} on ${TM_DATE}.
+ * Copyright (c) ${TM_YEAR} All rights reserved.
+ * 
+ */
+
+class ${TM_NEW_FILE_BASENAME} extends ModelAdmin {
+  	public static $managed_models = array(
+		
+	);
+	
+	static $url_segment = '${TM_NEW_FILE_BASENAME}';
+	
+	static $menu_title = '${TM_NEW_FILE_BASENAME}';
+}
